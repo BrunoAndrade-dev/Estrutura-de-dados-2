4e7d9abb7a1a32852233f24d72cba53042103fd6
Bruno Raphael <brunorafha31@gmail.com>
2026-04-16 11:49:04 -0300

    Add bubble sort implementation in BubbleSort.c
    Implement bubble sort algorithm to sort an array.

---
+++ BubbleSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void bubbleSort(int *vetor, int n) {
+    for (int i = 1 ; i < n , i++) {
+        for (int j = 0 ; j<n-1, j++){
+            if (v[j]> v[j+1]){
+                int x = v[j];
+                v[j] = v[j+1];
+                v[j+1] = x;
+            }
+
+        }
+    }
+
+}
